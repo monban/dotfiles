c4571d87b591b0fbc6d98531b0c310e97c8ee4ca
monban <himitsu.monban@gmail.com>
2015-08-26 20:56:05 -0400

    added script to start streaming to livecoding.tv

---
+++ livecode.sh
@@ -0,0 +1,19 @@
+INRES="1920x1080" 
+OUTRES="1920x1080" 
+FPS="15" 
+THREADS="0" 
+CBR="1500k" 
+QUALITY="veryfast" 
+AUDIO_RATE="44100"
+URI=rtmp://usmedia3.livecoding.tv:1935/livecodingtv/$LIVECODING_STREAM_KEY
+
+xrandr --fb $INRES
+ffmpeg \
+	-filter_complex "[0][1]overlay=main_w-overlay_w-32:main_h-overlay_h-32" \
+	-thread_queue_size 512 -s $INRES -r $FPS -f x11grab -i :0.0 \
+	-thread_queue_size 512 -input_format yuv420p -framerate $FPS -video_size 320x240 -f v4l2 -i /dev/video0 \
+	-thread_queue_size 512 -f alsa -ac 1 -ar $AUDIO_RATE -i default:CARD=Intel \
+	-pix_fmt yuv420p -vcodec libx264 -preset $QUALITY -maxrate $CBR -bufsize $CBR \
+	-s $OUTRES -threads $THREADS -acodec mp3 -af "highpass=f=100, lowpass=f=3000" -t 10 \
+	"$URI"
+xrandr --auto
