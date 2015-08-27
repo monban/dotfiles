9a4326b2edc4dc4ece21b8187a9c61a64c32eb22
monban <himitsu.monban@gmail.com>
2015-08-27 01:13:53 -0400

    lower bitrate, use libfdk_aac for sound encoding

--- livecode.sh
+++ livecode.sh
@@ -2,7 +2,7 @@ INRES="1920x1080"
 OUTRES="1920x1080" 
 FPS="15" 
 THREADS="0" 
-CBR="1500k" 
+CBR="900k" 
 QUALITY="veryfast" 
 AUDIO_RATE="44100"
 URI=rtmp://usmedia3.livecoding.tv:1935/livecodingtv/$LIVECODING_STREAM_KEY
@@ -10,10 +10,11 @@ URI=rtmp://usmedia3.livecoding.tv:1935/livecodingtv/$LIVECODING_STREAM_KEY
 xrandr --fb $INRES
 ffmpeg \
 	-filter_complex "[0][1]overlay=main_w-overlay_w-32:main_h-overlay_h-32" \
+	-threads $THREADS \
 	-thread_queue_size 512 -s $INRES -r $FPS -f x11grab -i :0.0 \
 	-thread_queue_size 512 -input_format yuv420p -framerate $FPS -video_size 320x240 -f v4l2 -i /dev/video0 \
 	-thread_queue_size 512 -f alsa -ac 1 -ar $AUDIO_RATE -i default:CARD=Intel \
-	-pix_fmt yuv420p -vcodec libx264 -preset $QUALITY -maxrate $CBR -bufsize $CBR \
-	-s $OUTRES -threads $THREADS -acodec mp3 -af "highpass=f=100, lowpass=f=3000" -t 10 \
+	-c:a libfdk_aac -b:a 64k -af "highpass=f=100, lowpass=f=3000" \
+	-s $OUTRES -f flv -pix_fmt yuv420p -c:v libx264 -preset $QUALITY -maxrate $CBR -bufsize $CBR \
 	"$URI"
 xrandr --auto
