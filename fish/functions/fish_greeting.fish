0071bad0f3668a531307de2a1884bdef4c8c54b9
Ian Flynn <himitsu.monban@gmail.com>
2021-11-02 22:49:12 -0400

    embed logo

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,5 +1,15 @@
 function fish_greeting
     if type -q lolcat; and test -e $HOME/itf_large.txt
-        lolcat $HOME/itf_large.txt
+        echo '
+          _________________________________________
+         /                                        /
+        /____     _________     ______     ______/
+            /    /        /    /     /    /
+           /    /        /    /     /    /___
+          /    /        /    /     /        /
+      ___/    /___     /    /     /    ____/
+     /           /    /    /     /    /
+    /___________/    /____/     /____/
+        '| lolcat -F 0.25
     end
 end
